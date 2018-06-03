7ad6c89b686597395fd3fd5cffbd9927d2ff4888
gauri <gauri.ramesh15@gmail.com>
2018-06-03 00:07:20 -0500

    attempted implementation of search functionality

---
+++ assets/js/search.js
@@ -0,0 +1,16 @@
+let search = () => {
+    var name = document.getElementById("query").value.toString();
+    var pattern = name.toLowerCase();
+    var targetId = "";
+  
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+       var para = divs[i].getElementsByTagName("p");
+       var index = para[0].innerText.toLowerCase().indexOf(pattern);
+       if (index != -1) {
+          targetId = divs[i].parentNode.id;
+          document.getElementById(targetId).scrollIntoView();
+          break;
+       }
+    }  
+ }
